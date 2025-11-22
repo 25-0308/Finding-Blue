ad3b98eb775734e56fef477214e57dcbd302766f
25-0308 <00rhkrtmdwns@hanmail.net>
2025-11-22 22:37:57 +0900

    함수 전반적 수정

--- FindingBlue/FindingBlue/Collision.h
+++ FindingBlue/FindingBlue/Collision.h
@@ -1,37 +1,23 @@
 #pragma once
 #include <GL/glm/glm.hpp>
-#include <vector>
-#include <memory>
+#include <Gl/glm/gtc/quaternion.hpp>
 
 class Collision 
 {
 public:
-	enum Coll_Type {
-		AABB,// 축정렬 바운딩 박스
-		OBB // 기본 바운딩 박스
-	};
+	glm::vec3 center = glm::vec3(0.0f); // 중심점 좌표
+	glm::vec3 halfsize = glm::vec3(1.0f, 1.0f, 1.0f); // 절반 사이즈
 
-	Collision(Coll_Type t) : type(t), isTrigger(false) {}
-	virtual ~Collision() = default;
+	Collision() = default;
+	explicit Collision(const glm::vec3 half); 
 
-	Coll_Type getType() const { return type; } // 타입값 반환
-	glm::vec3 getCenter() const { return center; } // 중심점 좌표 반환
-	void SetCenter(const glm::vec3& c) { center = c; } // 중심점 좌표 설정
+	glm::vec3 Min() const; // 최소 좌표 반환
+	glm::vec3 Max() const; // 최대 좌표 반환
 
-	bool IsTrigger() const { return isTrigger; } // 충돌 여부 반환
-	void SetTrigger(bool trigger) { isTrigger = trigger; } // 충돌 여부 설정
+	bool check_collision(const Collision& other) const; // 충돌 검사
 
-	virtual bool Check_COLL(const Collision* other) const = 0; // 충돌 체크
+	void updateBox(const glm::vec3& localHalfsize, 
+		const glm::quat& rotation, const glm::vec3& position = glm::vec3(0)); // 이동에 따른 업데이트
 
-	virtual void Debug_Draw() const = 0; // 바운딩박스 그리기
-
-protected:
-	Coll_Type type; // 바운딩 박스 타입
-	glm::vec3 center; // 오브젝트 중심
-	bool isTrigger = false; // 충돌 여부
+	void Debug_Draw(const glm::vec3& color = glm::vec3(1.0f, 0.0f, 0.0f)) const; // 빨간 박스 그리기
 };
-
-namespace Debug_Draw {
-	void Add_Line(const glm::vec3& a, const glm::vec3& b, const glm::vec3& color = glm::vec3(1.0f,0.0f,0.0f)); // 빨간 선으로 
-	void Render(); // 그리기
-}
