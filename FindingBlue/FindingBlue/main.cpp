733b9165f440d3cd4e54f0bad63c32fe67792196
25-0308 <00rhkrtmdwns@hanmail.net>
2025-12-08 12:44:40 +0900

    c 눌러서 콜라이더 on / off

--- FindingBlue/FindingBlue/main.cpp
+++ FindingBlue/FindingBlue/main.cpp
@@ -73,6 +73,7 @@ AIRPLANE* airplane;
 std::vector<MISSILE*> missiles;
 //숫자
 NUMBER* number_display;
+bool draw_coll = false;
 
 //��
 std::vector<ENEMY*> enemies;
@@ -426,8 +427,7 @@ GLvoid drawScene() {
 		1, GL_FALSE,
 		glm::value_ptr(MVP)
 	);
-
-	Debug_Draw::Render();
+	if(draw_coll) Debug_Draw::Render();
 	//UI그리는거 체력이랑 탄창
 	//UI는 평행투영
 	//뷰포트
@@ -639,6 +639,9 @@ GLvoid KeyboardDown(unsigned char key, int x, int y) {
 		break;
 	case'n':
 		game_clear = true;
+		break;
+	case'c':
+		draw_coll = !draw_coll;
 		break;
 	case 'q':
 		exit(0);
